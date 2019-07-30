928c4d4a9d905b1c6e9cb59ad999633b8cc5ea42
siarhei.baranouski <siarhei.baranouski@itechart-group.com>
2019-07-30 18:45:33 +0300

    erwer

--- scripts/create_root_dir.sh
+++ scripts/create_root_dir.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
 sudo mkdir /var/www -p
-sudo ls
-sudo pwd
-sudo cd ./deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive
-sudo unzip ./artifact.zip -d ./artifact
-sudo ls
--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 cd /var/www
 npm i --production
+sudo ls
+sudo pwd
+sudo unzip ./artifact.zip -d ./
+sudo ls
