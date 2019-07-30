3eb8dd18d1eb256af442e2c4aa54c5603ec652a7
siarhei.baranouski <siarhei.baranouski@itechart-group.com>
2019-07-30 18:25:40 +0300

    Added test scripts.

--- scripts/create_root_dir.sh
+++ scripts/create_root_dir.sh
@@ -3,6 +3,6 @@ sudo mkdir /var/www -p
 sudo ls
 sudo pwd
 sudo env
-sudo cd $DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID
+sudo cd ./$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID
 sudo pwd
 sudo unzip ./artifact.zip -d ./artifact
