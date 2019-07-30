55364d7df0e91981067970e2dccad1676d6c1613
siarhei.baranouski <siarhei.baranouski@itechart-group.com>
2019-07-30 18:31:03 +0300

    Added test scripts.

--- scripts/create_root_dir.sh
+++ scripts/create_root_dir.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
 sudo mkdir /var/www -p
-sudo ls
-sudo pwd
-sudo env
-sudo cd ./$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID
-sudo pwd
-sudo unzip ./artifact.zip -d ./artifact
--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 cd /var/www
+sudo ls
+sudo pwd
+sudo unzip ./artifact.zip -d ./
+sudo ls
 npm i --production
