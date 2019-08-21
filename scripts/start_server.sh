4913efc8ac3130636d7c1bdeff6f1b627267073e
siarhei.baranouski <siarhei.baranouski@itechart-group.com>
2019-08-21 11:16:59 +0300

    Added NODE_ENV.

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 cd /var/www
+if [ "$DEPLOYMENT_GROUP_NAME" == "ec2-ubuntu-test-staging" ]
+then
+    export NODE_ENV=staging
+else
+    export NODE_ENV=develop
+fi
+
 pm2 start --name server npm -- run start:ci
